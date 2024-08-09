c84881dea4727f208975f3697b2a1f2c3b589609
Jairo Vega <jvegacerda@gmail.com>
2024-08-09 01:26:27 -0400

    actualizado tarea hito 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,19 @@ import './App.css'
 import Producto from '../components/navbar'
 import Home from '../components/home'
 import Footer from '../components/footer'
+import Register from '../components/Register'
+import Login from '../components/Login'
 
 function App() {
 
   return (
     <>
       <Producto></Producto>
-      <Home></Home>
+      {/* <Home></Home> */}
+      <Register></Register>
+      <Login></Login>
       <Footer></Footer>
+      
     </>
   )
 }
